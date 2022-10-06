f22640926cb84965c3fc4dcab6af092b6b9b37c4
Quentin VOITURON <quentin.voituron@gadz.org>
2022-10-06 14:35:18 +0200

    chenge the merge separately

--- src/cube.py
+++ src/cube.py
@@ -68,6 +68,9 @@ if __name__ == "__main__":
     era_filter_daily = hz.resample_to_daily(era_filter)
     #Projection of the era into sinuoidal projection
     era_sinu = era_filter_daily.rio.reproject(crs_sinu)
+    # Renaming dimensions of era data set to match the other data sets
+    #era_filter_proj = era_sinu.rename({'y': 'ydim', 'x': 'xdim'})
+
     # Regrid the era data to the common grid
     era_filter_proj = hz.interpolate_to_common_grid(era_sinu, common_grid)
 
@@ -96,28 +99,35 @@ if __name__ == "__main__":
     # Subset all dataset from the list using sel method to '2010-02-01', '2022-01-01'
     data_sets = [ds.sel(time=slice('2010-02-01', '2022-01-01')) for ds in data_sets]
 
-
-
-    #Append the density data set to the list
-    data_sets.append(density_proj)
-
     # Rename dataArrays before merging
     density_proj.name = 'density'
 
-    # Renaming dimensions of era data set to match the other data sets
-    era_filter_proj = era_filter_proj.rename({'y': 'ydim', 'x': 'xdim'})
+    # change density_proj to xarray dataset
+    density_proj = density_proj.to_dataset()
     # Renaming dimensions of density to match the other data sets
-    density_proj = density_proj.rename({'y': 'ydim', 'x': 'xdim'})
+    #density_proj = density_proj.rename({'y': 'ydim', 'x': 'xdim'})
 
+    #Append the density data set to the list
+    # Create a first list with coordinate x and y
+    list_xy = [lai_filter_proj_daily, evap_filter_proj_daily, era_filter_proj, density_proj]
+    list_xdimydim = [ndvi_filter_daily, burn_mask_filter_daily, active_fire_filter_proj_daily]
+
+    # Merge and save by coordinates the data sets from the lists
+    xr.combine_by_coords(list_xy, combine_attrs='drop_conflicts').to_netcdf(path_data+'list_xy.nc')
+    xr.combine_by_coords(list_xdimydim, combine_attrs='drop_conflicts').to_netcdf(path_data+'list_xdimydim.nc')
 
 
 
 
     # Merge by coordinates the data sets
-    data_merged = xr.combine_by_coords(data_sets, combine_attrs='drop_conflicts')
+    #data_merged = xr.combine_by_coords(data_sets, combine_attrs='drop_conflicts')
+    #data_merged = xr.combine_by_coords(data_sets, combine_attrs='drop_conflicts')
 
     # Save the data set
-    data_merged.to_netcdf(path_data + 'data_merged.nc')
+    #data_merged.to_netcdf(path_data + 'data_merged_semi.nc')
+    #era_filter_proj.to_netcdf(path_data + 'weather.nc')
+    #density_proj.to_netcdf(path_data + 'density.nc')
+
 
 
 
