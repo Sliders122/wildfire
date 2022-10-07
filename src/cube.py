d40de165c0018c1c32c4f0302780799dc8155313
Quentin VOITURON <quentin.voituron@gadz.org>
2022-10-07 19:45:51 +0200

    updated comments

--- src/cube.py
+++ src/cube.py
@@ -31,11 +31,22 @@ if __name__ == "__main__":
     lst_night_filter = lst_night['LST_Night_1km']
     lst_day_filter = lst_day['LST_Day_1km']
     # fwi_filter = fwi['fwi-daily-proj']
-    active_fire_filter = active_fire[['First_Day', 'Last_Day']]
+    active_fire_filter = active_fire[['First_Day', 'Last_Day', 'Burn_Date']]
     burn_mask_filter = burn_mask['FireMask']
 
     # Convert era calendar to cftime.DatetimeJulian
     era_filter = era_filter.convert_calendar('julian')
+    # Subset the data sets to the same time period: 2010-01-01 to 2021-01-01
+    ndvi_filter = ndvi_filter.sel(time=slice('2010-01-01', '2021-01-01'))
+    lai_filter = lai_filter.sel(time=slice('2010-01-01', '2021-01-01'))
+    evap_filter = evap_filter.sel(time=slice('2010-01-01', '2021-01-01'))
+    era_filter = era_filter.sel(time=slice('2010-01-01', '2021-01-01'))
+    lst_night_filter = lst_night_filter.sel(time=slice('2010-01-01', '2021-01-01'))
+    lst_day_filter = lst_day_filter.sel(time=slice('2010-01-01', '2021-01-01'))
+    # fwi_filter = fwi_filter.sel(time=slice('2010-01-01', '2021-01-01'))
+    active_fire_filter = active_fire_filter.sel(time=slice('2010-01-01', '2021-01-01'))
+    burn_mask_filter = burn_mask_filter.sel(time=slice('2010-01-01', '2021-01-01'))
+
 
     # Create a CRS object from a poj4 string for sinuoidal projection
     crs_sinu = rasterio.crs.CRS.from_string(
@@ -97,7 +108,7 @@ if __name__ == "__main__":
 
     # Different method to interpolate the active fire data set
     active_fire_filter_proj = active_fire_filter.interp(ydim=ndvi["ydim"], xdim=ndvi['xdim'])
-    #Fitting floats to the original dataset
+
 
 
 
@@ -113,32 +124,28 @@ if __name__ == "__main__":
     data_sets = [ndvi_filter_daily, burn_mask_filter_daily, lai_filter_proj_daily, evap_filter_proj_daily, era_filter_proj, active_fire_filter_proj_daily]
 
     # Subset all dataset from the list using sel method to '2010-02-01', '2022-01-01'
-    data_sets = [ds.sel(time=slice('2010-02-01', '2022-01-01')) for ds in data_sets]
+    data_sets = [ds.sel(time=slice('2011-02-01', '2021-01-01')) for ds in data_sets]
 
 
-    # Renaming dimensions of density to match the other data sets
-    #density_proj = density_proj.rename({'y': 'ydim', 'x': 'xdim'})
-
-    #Append the density data set to the list
     # Create a first list with coordinate x and y
     list_xy = [lai_filter_proj_daily, evap_filter_proj_daily, era_filter_proj, density_proj]
     list_xdimydim = [ndvi_filter_daily, burn_mask_filter_daily, active_fire_filter_proj_daily]
 
     # Merge and save by coordinates the data sets from the lists
-    xr.combine_by_coords(list_xy, combine_attrs='drop_conflicts').to_netcdf(path_data+'list_xy.nc')
-    xr.combine_by_coords(list_xdimydim, combine_attrs='drop_conflicts').to_netcdf(path_data+'list_xdimydim.nc')
+    ds_xy = xr.combine_by_coords(list_xy, combine_attrs='drop_conflicts')
+    ds_xdimydim = xr.combine_by_coords(list_xdimydim, combine_attrs='drop_conflicts')
 
+    # Match the coordinates values of the data sets
+    ds_xdimydim_xdimydim = ds_xdimydim.assign_coords(xdim=ds_xy.coords['x'].values, ydim=ds_xy.coords['y'].values)
 
+    # Renaming the coordinates of the data sets to match the other data sets
+    ds_xdimydim_xdimydim = ds_xdimydim_xdimydim.rename({'xdim': 'x', 'ydim': 'y'})
 
-
-    # Merge by coordinates the data sets
-    #data_merged = xr.combine_by_coords(data_sets, combine_attrs='drop_conflicts')
-    #data_merged = xr.combine_by_coords(data_sets, combine_attrs='drop_conflicts')
+    # Merge the data sets
+    ds = xr.merge([ds_xy, ds_xdimydim_xdimydim])
 
     # Save the data set
-    #data_merged.to_netcdf(path_data + 'data_merged_semi.nc')
-    #era_filter_proj.to_netcdf(path_data + 'weather.nc')
-    #density_proj.to_netcdf(path_data + 'density.nc')
+    ds.to_netcdf(path_data+'datacube.nc')
 
 
 
