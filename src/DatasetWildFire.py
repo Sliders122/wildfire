f525c20ab188c024a8217c6fbb9e848e427c9d7b
saschamoccozet <saschamoccozet.pro@posteo.net>
2022-11-13 15:57:20 +0100

    refector fbfill

--- src/DatasetWildFire.py
+++ src/DatasetWildFire.py
@@ -123,7 +123,7 @@ class DatasetWildFire(xr.Dataset):
                f'\n\t  percentage : {self[variable].isnull().sum().values / self[variable].size * 100}' \
                f'\n\t  count : {self[variable].isnull().sum().values.ravel()[0]} / {self[variable].size}'
 
-    def stat_missing_data(self) -> string:
+    def stat_print_missing_data(self) -> string:
         """
         Statistical methods used to print the percentage and number of missing data for every variables of the Dataset
 
@@ -135,3 +135,76 @@ class DatasetWildFire(xr.Dataset):
         for var in list(self.keys()):
             _tmp += f'\n\t{self.stat_print_missing_data_variable(var)}'
         return _tmp
+
+
+    def fbfill(self
+               , variables: list = []
+               , dims: list = ["x", "y", "time"]
+               , flimit: int = 1
+               , blimit: int = 1
+               , max_occurence: int = -1
+               ):
+        """
+        A recursive function that remove the nan values on a datacube.
+
+        It first apply a forward fill with a limit of *flimit*
+        Then a bakward fill with a limit of *blimit*
+        on the *coords* of a *datacube*
+        untill there is no more nan or until *max_occurence* is reached
+
+        Parameters
+        ----------
+        variables : list, default []
+            Specify upon which variable of the dataset we want to fill.
+            if empty it will contain all the variables of the dataset.
+
+        dims : list, default ["x", "y", "time"]
+            Specifies the dimension along which to propagate values when filling.
+
+        flimit : integer, default 1
+            The maximum number of consecutive NaN values to forward fill.
+            In other words, if there is a gap with more than this number of consecutive NaNs,
+            it will only be partially filled.
+            Must be greater than 0 or None for no limit.
+            Must be None or greater than or equal to axis length if filling along chunked axes (dimensions).
+
+        blimit : integer, default 1
+            The maximum number of consecutive NaN values to backward fill.
+            In other words, if there is a gap with more than this number of consecutive NaNs,
+            it will only be partially filled.
+            Must be greater than 0 or None for no limit.
+            Must be None or greater than or equal to axis length if filling along chunked axes (dimensions).
+
+        max_occurence : integer, default -1
+            The maximum number of time the filling will be processed.
+            If the number is negative the filling will be done untill there is no more Nan value in the datacube.
+
+        """
+
+        #if variables is empty it means the filling will be done upon all the variables of the dataset
+        if len(variables) == 0:
+            variables = list(self.keys())
+
+        _datacube = self.copy()
+
+        # for all the variables
+        for _var in variables:
+            # on all the variable dimension
+            for _dim in self[_var].dims:
+                # if the variables dimension is in the targeted dimensions
+                if _dim in dims:
+                    self[_var] = self[_var].ffill(_dim, limit=flimit)
+                    self[_var] = self[_var].bfill(_dim, limit=blimit)
+
+        # # If the original datacube has not been changed from the copy after the filling
+        # # or if the maximum occurence wanted is reached
+        if self.equals(_datacube) or max_occurence == 0:
+            return None
+
+        # recursive on the _datacube
+        return self.fbfill(variables = variables
+                           , dims=dims
+                           , flimit=flimit
+                           , blimit=blimit
+                           , max_occurence=max_occurence - 1
+                      )
