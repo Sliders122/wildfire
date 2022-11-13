ebd42276fe73f6e42a9be1c2dab55bbfb88c4b38
Quentin VOITURON <quentin.voituron@gadz.org>
2022-11-13 18:59:13 +0100

    script to create the dataframe

--- src/dataframe.py
+++ src/dataframe.py
@@ -13,22 +13,49 @@ if __name__ == "__main__":
     # Load the data set
     datacube = xr.open_dataset(path_data + 'datacube.nc')
 
+    # Subset only for the month june, July, August of each year
+    datacube = datacube.sel(time=datacube['time.month'].isin([6, 7, 8]))
+
+    # Subset for training
+    datacube_train = datacube.sel(time=slice('2010-01-01', '2016-12-31'))
+    # Subset for testing
+    datacube_test = datacube.sel(time=slice('2017-01-01', '2021-01-01'))
+
     # To dataframe
-    df = datacube.to_dataframe()
+    df_train = datacube_train.to_dataframe()
+    df_test = datacube_test.to_dataframe()
+
+    # Drop the columns we don't need: crs, band, spatial_ref
+    df_train = df_train.drop(columns=['crs', 'band', 'spatial_ref'])
+    df_test = df_test.drop(columns=['crs', 'band', 'spatial_ref'])
 
     # Remove the NaN values
-    df = df.dropna()
+    df_train = df_train.dropna()
+    df_test = df_test.dropna()
 
-    # Change the value of FireMAsk to 0 and 1. 0 = no fire, 1 = fire . If FireMask is 7,8,9 then it is a fire. If not, it is not a fire.
-    df['FireMask'] = df['FireMask'].replace([0, 1, 2, 3, 4, 5, 6], 0)
-    df['FireMask'] = df['FireMask'].replace([7, 8, 9], 1)
+    # Change the value of FireMAsk to 0 and 1. 0 = no fire, 1 = fire . If FireMask is 7,8,9 then it is a fire. If
+    # not, it is not a fire.
+    df_train['FireMask'] = df_train['FireMask'].replace([0, 1, 2, 3, 4, 5, 6], 0)
+    df_train['FireMask'] = df_train['FireMask'].replace([7, 8, 9], 1)
+    df_test['FireMask'] = df_test['FireMask'].replace([0, 1, 2, 3, 4, 5, 6], 0)
+    df_test['FireMask'] = df_test['FireMask'].replace([7, 8, 9], 1)
 
-    # Create an object which counts the number of fire
-    fire_count = df.groupby('FireMask').count()
+    # Create an object which evaluates the length of FireMask==1
+
+    # Sum the number of fires in the training data and change into an integer
+    num_fires_train = int(df_train['FireMask'].sum())
+    # Sum the number of fires in the testing data and change into an integer
+    num_fires_test = int(df_test['FireMask'].sum())
+
+
 
     # Keep all the observations with FireMask = 1 and keep 'fire_count' observations with FireMask = 0 randomly
-    df = df[df['FireMask'] == 1].append(df[df['FireMask'] == 0].sample(n=fire_count['FireMask'][1]))
+    df_train_balanced = df_train[df_train['FireMask'] == 1].append(
+        df_train[df_train['FireMask'] == 0].sample(n=num_fires_train, random_state=1))
+    df_test_balanced = df_test[df_test['FireMask'] == 1].append(
+        df_test[df_test['FireMask'] == 0].sample(n=num_fires_test, random_state=1))
 
-    # Save the dataframe
-    df.to_csv(path_data + 'dataframe.csv')
-
+    # Save the dataframes df_train_balanced and df_test_balanced, df_test as csv files
+    df_train_balanced.to_csv(path_data + 'df_train_balanced.csv')
+    df_test_balanced.to_csv(path_data + 'df_test_balanced.csv')
+    df_test.to_csv(path_data + 'df_test_imbalanced.csv')
