e72851a7acb39f52b3da9cfc4b2b8ef6099e8f80
saschamoccozet <saschamoccozet.pro@posteo.net>
2022-12-06 15:06:35 +0100

    add dashboard to git

---
+++ src/deployement/dashboard.py
@@ -0,0 +1,18 @@
+import pickle
+import pandas as pd
+from explainerdashboard import ClassifierExplainer, ExplainerDashboard
+
+#load the data
+df = pd.read_csv("df_model.csv")
+
+# Split the test data
+df_xtest = df.drop(['index','FireMask','time','FireMask_pred','FireMask_proba'], axis=1)
+df_ytest = df['FireMask']
+
+#load the model
+model_path = open('model_lgbm.pkl' , 'rb')
+model = pickle.load(model_path)
+
+# Create and run the dashboard on port 9050
+explainer = ClassifierExplainer(model, df_test, df_train)
+ExplainerDashboard(explainer).run(port=9050)
